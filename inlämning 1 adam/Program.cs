0346fe36f5025f33fedb126fd85037195a659da6
adhi08001 <adhi08001@E-PW04SM21>
2025-09-03 13:14:52 +0200

    Add project files.

---
+++ inlämning 1 adam/Program.cs
@@ -0,0 +1,11 @@
+﻿using System; 
+class Program
+{
+    static void Main()
+    {
+       
+        Console.WriteLine("Du är nästan klar med kapitel 1.");
+
+        Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\.");
+    }
+}
